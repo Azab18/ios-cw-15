73144a9f46444c46f74c44c909b9edfa2185371b
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2022-03-19 10:38:41 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Person{
+    let name: String
+    let age : Int
+    let degree: Double
+    let attendence: Bool
+}
+let Ziad: Person = Person(name: "ziad", age: 17, degree: 99, attendence: true)
+
